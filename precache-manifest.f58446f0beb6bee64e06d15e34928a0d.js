8a488b2f220b8e99906018df701ae4f6cbd87e40
Marcos Orive Izarra <marcos@orive.me>
2019-02-22 09:54:15 +0100

    Updates

---
+++ precache-manifest.f58446f0beb6bee64e06d15e34928a0d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb8a5a4d9dc3fd62b585",
+    "url": "/apod-react/static/js/runtime~main.fb8a5a4d.js"
+  },
+  {
+    "revision": "a601b5ece48f5995fec2",
+    "url": "/apod-react/static/js/main.a601b5ec.chunk.js"
+  },
+  {
+    "revision": "578d44b1d7375673b907",
+    "url": "/apod-react/static/js/2.578d44b1.chunk.js"
+  },
+  {
+    "revision": "3e40a5b850dfaaf2b0aec9feb53b24fb",
+    "url": "/apod-react/index.html"
+  }
+];
